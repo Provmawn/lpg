8b3e4599fd0bdab670bf0d8c81f79add962bc237
provmawn <provmawn@gmail.com>
2022-10-05 21:54:06 -0700

    add communication to steam api

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("hello world")
+	token := flag.String("token", "", "steam api token")
+	steamid := flag.String("steamid", "", "steamid")
+	flag.Parse()
+	fmt.Println(*token)
+	//response, err := http.Get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + *token + "&steamids=" + *steamid)
+	response, err := http.Get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + *token + "&steamid=" + *steamid + "&format=json")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	bodyBytes, err := io.ReadAll(response.Body)
+	fmt.Println(string(bodyBytes))
+
 }
