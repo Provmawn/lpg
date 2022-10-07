b4e3caf1d6434131ab4be5b3ea804561620f301b
provmawn <provmawn@gmail.com>
2022-10-06 20:04:41 -0700

    unmarshal api json data

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,61 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"time"
 )
 
-func main() {
-	token := flag.String("token", "", "steam api token")
-	steamid := flag.String("steamid", "", "steamid")
-	flag.Parse()
-	fmt.Println(*token)
-	//response, err := http.Get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + *token + "&steamids=" + *steamid)
-	response, err := http.Get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + *token + "&steamid=" + *steamid + "&format=json")
+/* none of this works
+type Response struct {
+	GamesCount int    `json:"games_count"`
+	Games      []Game `json:"games"`
+}
+
+type Game struct {
+	AppId                  int `json:"appid"`
+	PlaytimeForever        int `json:"playtime_forever"`
+	PlaytimeWindowsForever int `json:"playtime_windows_forever"`
+	PlaytimeMacForever     int `json:"playtime_mac_forever"`
+	PlaytimeLinuxForever   int `json:"playtime_linux_forever"`
+	RTimeLastPlayed        int `json:"rtime_last_played"`
+}
+
+*/
+
+func PrettyPrint(i interface{}) string {
+	s, _ := json.MarshalIndent(i, "", "  ")
+	return string(s)
+}
+
+func GetOwnedGames(token string, steamid string) map[string]interface{} {
+	// do api request to steam
+	var c = &http.Client{Timeout: 10 * time.Second}
+	r, err := c.Get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + token + "&steamid=" + steamid + "&format=json")
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
-	bodyBytes, err := io.ReadAll(response.Body)
-	fmt.Println(string(bodyBytes))
+	defer r.Body.Close()
 
+	// print out the body
+	bodyBytes, err := io.ReadAll(r.Body)
+	fmt.Println(len(string(bodyBytes)))
+
+	// decode the response
+	var result map[string]interface{}
+	json.Unmarshal(bodyBytes, &result)
+	return result
+}
+
+func main() {
+	token := flag.String("token", "", "steam api token")
+	steamid := flag.String("steamid", "", "steam ID")
+	flag.Parse()
+	games := GetOwnedGames(*token, *steamid)
+	fmt.Println(PrettyPrint(games))
 }
